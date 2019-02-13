fba75d71ac1f2a4dd59ed980fd85e7ab6e01b8cd
Anab Khan <anab.khan@softwareag.com>
2019-02-13 15:50:26 +0530

    Initial Draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DragNDropDemo';
+  
+  numbers: any[] = [];
+  otherNumbers: any[] = [];
+
+  scrollListNumbers:any[];
+
+  constructor() {
+    for (let index = 0; index < 5; index++) {
+      this.numbers.push({
+        index : index,
+        color: Math.floor(Math.random()*16777215).toString(16)
+      });
+    }
+
+    this.scrollListNumbers = Array(10000).fill(0);
+    
+  }
+
+  onItemDropped(event: CdkDragDrop<number[]>) {
+    // check if item is dropped to different container
+    if (event.previousContainer != event.container) {
+      // transfer item from one previous container array to current
+      transferArrayItem(event.previousContainer.data, event.container.data,event.previousIndex,event.currentIndex);
+    } else {
+      // move item within array from previous index to current
+      moveItemInArray(this.numbers, event.previousIndex, event.currentIndex);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
